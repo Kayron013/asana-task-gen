1946da3b420980fb8a78df0b86eff7d45702a19d
Angel <kayron013@gmail.com>
2020-01-14 20:00:58 -0500

    modify tasks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const user_gid = '1001152530532638';
 const proj_gid = '1133011438610423'; // Weekly Edits
 const dvd_section_gid = '1133011438610435'; // CD / DVD
 const yt_section_gid = '1133011438610436';
+const services_section_gid = '1157019523363033';
 const { createTask, addTaskToSection } = require('./requests');
 const moment = require('moment');
 
@@ -26,35 +27,28 @@ const dueDate = (date, day) => {
  */
 const generateTasks = async (date = moment().startOf('week')) => {
   try {
-    // Create DVD and YT tasks
-    const [{ gid: dvd_task_gid }, { gid: yt_task_gid }] = await Promise.all(
-      ['dvd', 'yt'].map(type =>
-        createTask({
-          assignee: user_gid,
-          projects: [proj_gid],
-          name: formatDate(date),
-          due_on: type === 'dvd' ? dueDate(date, 0) : dueDate(date, 3)
-        })
-      )
-    );
+    // Create task
+    const { gid: task_gid } = await createTask({
+      assignee: user_gid,
+      projects: [proj_gid],
+      name: `${formatDate(date)} Service`,
+      due_on: dueDate(date, 3)
+    });
 
     // Create subtasks
     await Promise.all(
-      ['8am', '11am'].map(name =>
+      ['8am', '11am', 'YT Upload', 'Topic Log Entry'].map(name =>
         createTask({
           assignee: user_gid,
-          parent: dvd_task_gid,
+          parent: task_gid,
           name,
-          due_on: name === '8am' ? dueDate(date, 0) : dueDate(date, 3)
+          due_on: dueDate(date, 3)
         })
       )
     );
 
-    // Add main tasks to corresponding sections
-    await Promise.all([
-      addTaskToSection(dvd_task_gid, dvd_section_gid),
-      addTaskToSection(yt_task_gid, yt_section_gid)
-    ]);
+    // Add main task to section
+    await addTaskToSection(task_gid, services_section_gid);
 
     console.log('Done');
   } catch (e) {
